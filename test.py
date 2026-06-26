1d5a6531f4f5dcc7a9b6a572c9adf8ba770547aa
Ireoluwa Michael Sholanke <mikethestemist@gmail.com>
2026-06-26 14:35:41 +0100

    Test for previous commit - Refactor email parsing and regex patterns

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
+### error was in the input email: initially didn't support N133.01 without the space after 'N'
 import re 
-emails ="""
-
+emails = """"
 MoniePoint
 Debit Alert
 Hi IREOLUWA, 
@@ -17,40 +17,14 @@ Date & Time:
 10 Jun, 2025 | 08:50:52 AM
 Narration: 
 Auto-Save 10\\% \\on Airtime Purchase for Save As You Transact.
+
 """
-### make multi-line text single-line
-text = ' '.join(emails.split())
+
+text = ''.join(emails)
 print(text, '\n')
 
-### get alert type
-alert_type = re.search(r'(\w+)\s+[Aa]lert', text).group()
-print(alert_type, '\n')
-
-
-# transaction_amount = re.findall(r'($|NG?N?\s\d*,?\d+.?\d+)', text)
-# transaction_amount = re.findall(r'[Aa]mount:?\s+($|NG?N?\s\d*,?\d+.?\d+)', text)
-
-### get transaction amount
-transaction_amount = re.search(r'([Dd]ebit|[Cc]redit|[Tt]ransaction)\s[Aa]mount:?\s+($|NG?N?\s\d*,?\d+.?\d+)', text).group(2)
+transaction_amount = re.search(r'([Dd]ebit|[Cc]redit|[Tt]ransaction)\s[Aa]mount:?\s+($|NG?N?\s*\d*,?\d+.?\d+)', text).group(2)
 print('Transaction amount:', transaction_amount, '\n')
 
-### get current account balanace 
-account_balance = re.search(r'[Bb]alance:?\s+($|NG?N?\s\d*,?\d+.?\d+)', text).group()
+account_balance = re.search(r'[Bb]alance:?\s+($|NG?N?\s*\d*,?\d+.?\d+)', text).group()
 print('Account balance:', account_balance, '\n')
-
-
-### get the transaction details/description/narration
-description = re.search(r'([Nn]arration|[Dd]etails|[Dd]escription):?\s+(\w+\s?(\w+)?\s?(\w+)?\s?(\w+)?\s?(\w+)?\s?)', text).group(2)
-print('Description:', description, '\n')
-
-
-### get date and time
-# Date & Time: 
-# 10 Jun, 2025 | 08:50:51 AM
-
-time = re.search(r'(\d+:\d+(:\d{2})?\s+(([Aa]|[Pp])[Mm])?)', text).group()
-print('Time of transaction:', time, '\n')
-
-# date_formats = ['20-06-2026', '20-06-26', '20/06/2026', '20/06/26', '20th Jun, 2026', 'January 20th, 2026']
-date = re.search(r'(\d{1,2}-\d{1,2}-\d{2,4})|(\d{1,2}\/\d{1,2}\/\d{2,4})|(\d{1,2}(st|nd|rd|th)?\s\w+,?\s\d{2,4})|(\w+\s\d{1,2}(st|nd|rd|th)?,?\s\d{2,4})', text).group()
-print('Date:', date, '\n')
